f114a2e154dafc2421d4bfbcca7f389146041496
Hongzhang Shan <hongzhang@fb.com>
2020-10-13 18:42:08 -0700

    modify linear
    Summary:
    Integrate pytorch_linear.py into driver.py
    [Have not fully tested yet]
    
    Reviewed By: amirstar
    
    Differential Revision: D24231618
    
    fbshipit-source-id: 775fcb4e1006d2748627b369d5c876ce32f698f2

--- train/compute/pt/pytorch_linear.py
+++ train/compute/pt/pytorch_linear.py
@@ -5,31 +5,9 @@
 
 import time
 
-import apex
-import click
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torch.utils.data import Dataset
-
-
-class myDataset(Dataset):
-    def __init__(
-        self, batch_size, num_batches, input_size, output_size, transform=None
-    ):
-        self.transform = transform
-        self.batch_size = batch_size
-        self.num_batches = num_batches
-        self.input_size = input_size
-        self.output_size = output_size
-
-    def __len__(self):
-        return self.batch_size * self.num_batches
-
-    def __getitem__(self, idx):
-        input_sample = torch.FloatTensor(self.input_size).uniform_(-1, 1)
-        output_label = torch.randint(0, self.output_size, (1,), dtype=torch.long)[0]
-        return input_sample, output_label
 
 
 class Net(nn.Module):
@@ -51,30 +29,15 @@ class Net(nn.Module):
         return x
 
 
-def train(
-    model,
-    device,
-    train_loader,
-    optimizer,
-    data_type,
-    batch_size,
-    num_batches,
-    input_size,
-    hidden_size,
-    output_size,
-    layer_num,
+def train_cpu(
+    model, device, optimizer, data_type, input_size, output_size, batch_size, args
 ):
+    loss_f = nn.CrossEntropyLoss()
 
-    loss_f = nn.CrossEntropyLoss().to(device)
-
-    if data_type == "float16":
-        model = apex.fp16_utils.network_to_half(model)
-
-    model.train()
+    # model.train()
     start_time = time.time()
 
-    # for batch_idx, (data, target) in enumerate(train_loader):
-    for _ in range(num_batches):
+    for i in range(args.steps + args.warmups):
         data = torch.randn(batch_size, input_size, device=device)
         target = torch.randint(
             output_size, [batch_size], device=device, dtype=torch.long
@@ -88,103 +51,262 @@ def train(
         loss = loss_f(output, target)
         loss.backward()
         optimizer.step()
-    total_time = time.time() - start_time
-    total_examples = batch_size * num_batches
+        if i < args.warmups:
+            start_time = time.time()
 
-    print("---------------------------------------------")
-    print("QPS: {:.6f}".format(total_examples / total_time))
-    print("global_step/sec: {:.6f}".format(num_batches / total_time))
-    print("Total time: {:.6f}".format(total_time))
-    flops = batch_size * (
-        hidden_size * hidden_size * layer_num
-        + hidden_size * input_size
-        + hidden_size * output_size
-    )
-    # Forward 2x and Backward 4x
-    flops *= 6 * num_batches
-    print("TFLOPS: {}".format(flops / total_time / 1e12))
+    return time.time() - start_time, loss
 
 
-@click.command()
-@click.option("--use-gpu/--no-use-gpu", default=True, help="Use GPU/CPU")
-@click.option("--layer-num", type=int, default=20, help="Number of Linear layers")
-@click.option("--batch-size", type=int, default=512, help="Batch size")
-@click.option("--input-size", type=int, default=1024, help="Input layer size")
-@click.option(
-    "--hidden-size", type=int, default=128, help="Number of hidden_sizes per layer"
-)
-@click.option("--output-size", type=int, default=1024, help="Output layer size")
-@click.option("--num-batches", type=int, default=100, help="Number of batches to train")
-@click.option(
-    "--data-type",
-    default="float16",
-    type=click.Choice(["float", "float16"]),
-    help="data type",
-)
-@click.option(
-    "--optimizer-type",
-    default="sgd",
-    type=click.Choice(["sgd"]),
-    help="Optimizers: SGD",
-)
-def main(
-    use_gpu,
-    layer_num,
-    batch_size,
-    input_size,
-    hidden_size,
-    output_size,
-    num_batches,
-    data_type,
-    optimizer_type,
+def train_gpu(
+    model, device, optimizer, data_type, input_size, output_size, batch_size, args
 ):
-    # print("PyTorch VERSION:", torch.__version__)
-    # print("CuDNN version: {}".format(torch.backends.cudnn.version()))
-    # print("PyTorch config: {}".format(torch.__config__.show()))
+    import apex
+
+    loss_f = nn.CrossEntropyLoss().to(device)
+
+    if data_type == "float16":
+        model = apex.fp16_utils.network_to_half(model)
+
+    # model.train()
+    start_time = time.time()
+
+    for i in range(args.steps + args.warmups):
+        data = torch.randn(batch_size, input_size, device=device)
+        target = torch.randint(
+            output_size, [batch_size], device=device, dtype=torch.long
+        )
+        # data, target = data.to(device), target.to(device)
+        if data_type == "float16":
+            data = data.half()
+
+        optimizer.zero_grad()
+        output = model(data).float()
+        loss = loss_f(output, target)
+        loss.backward()
+        optimizer.step()
+        if i < args.warmups:
+            start_time = time.time()
+
+    return time.time() - start_time, loss
+
+
+def train_tpu(
+    model, device, optimizer, data_type, input_size, output_size, batch_size, args
+):
+    import torch_xla.core.xla_model as xm
+
+    loss_f = nn.CrossEntropyLoss().to(device)
+
+    # model.train()
+    start_time = time.time()
+
+    for i in range(args.steps + args.warmups):
+        data = torch.randn(batch_size, input_size, device=device)
+        target = torch.randint(
+            output_size, [batch_size], device=device, dtype=torch.long
+        )
+        # data, target = data.to(device), target.to(device)
+
+        optimizer.zero_grad()
+        output = model(data).float()
+        loss = loss_f(output, target)
+        loss.backward()
+        optimizer.step()
+        xm.mark_step()
+        if i < args.warmups:
+            start_time = time.time()
+
+    return time.time() - start_time, loss
+
+
+def train(
+    model, device, optimizer, data_type, input_size, output_size, batch_size, args
+):
+
+    if device.type == "cpu":
+        elap, loss = train_cpu(
+            model,
+            device,
+            optimizer,
+            data_type,
+            input_size,
+            output_size,
+            batch_size,
+            args,
+        )
+
+    elif device.type == "cuda":
+        elap, loss = train_gpu(
+            model,
+            device,
+            optimizer,
+            data_type,
+            input_size,
+            output_size,
+            batch_size,
+            args,
+        )
+
+    elif device.type == "xla":
+        elap, loss = train_tpu(
+            model,
+            device,
+            optimizer,
+            data_type,
+            input_size,
+            output_size,
+            batch_size,
+            args,
+        )
+
+    return elap, loss
+
+
+def run_single(args, layer_num, input_size, hidden_size, output_size, batch_size):
+
+    device = args.device
+    optimizer_type = args.optimizer_type
+    data_type = args.dtype
+    # num_batches = args.num_batches
+
+    dt = torch.float32
+    if data_type == "float16":
+        dt = torch.float16
+    if data_type == "bfloat16":
+        dt = torch.bfloat16
 
     torch.manual_seed(1)
 
-    kwargs = {"num_workers": 1, "pin_memory": True} if use_gpu else {}
-    train_loader = torch.utils.data.DataLoader(
-        myDataset(batch_size, num_batches, input_size, output_size),
-        batch_size=batch_size,
-        shuffle=True,
-        **kwargs
+    lr = 0.01
+
+    if device == "cpu":
+        dev = torch.device("cpu")
+        model = Net(input_size, hidden_size, output_size, layer_num).to(dev)
+        if optimizer_type == "sgd":
+            optimizer = torch.optim.SGD(model.parameters(), lr=lr)
+        else:
+            assert 0, "Unsupported optimizer type"
+
+    elif device == "gpu":
+
+        assert torch.cuda.is_available(), "cuda not available"
+
+        import apex
+
+        dev = torch.device("cuda:0")
+        model = Net(input_size, hidden_size, output_size, layer_num).to(dev)
+        if optimizer_type == "sgd":
+            optimizer = apex.optimizers.FusedSGD(
+                model.parameters(), lr=lr, set_grad_none=True
+            )
+        else:
+            assert 0, "Unsupported optimizer type"
+
+        if data_type == "float16":
+            apex.amp.initialize(model, optimizer, opt_level="O2", verbosity=0)
+
+    elif device == "tpu":
+
+        import torch_xla.core.xla_model as xm
+
+        # alldev = xm.get_xla_supported_devices()
+        # allrealdev = xm.xla_real_devices(alldev)
+        # print("Found {0} XLA devices: {1}".format(len(allrealdev), allrealdev))
+
+        dev = xm.xla_device()
+        # print("Using device:", dev)
+        model = Net(input_size, hidden_size, output_size, layer_num).to(dev)
+        if optimizer_type == "sgd":
+            optimizer = torch.optim.SGD(model.parameters(), lr=lr)
+        else:
+            assert 0, "Unsupported optimizer type"
+
+    elap, loss = train(
+        model, dev, optimizer, dt, input_size, output_size, batch_size, args
+    )
+    return elap, loss
+
+
+def run(args, dataset):
+
+    print(
+        "--------------------------------------------------------------------------------"
+    )
+    print(
+        " #Layer    Input    Hidden    Output   Batch   Time(s)/step  QPS      Rate(TF/s)"
+    )
+    print(
+        "--------------------------------------------------------------------------------"
     )
 
-    use_gpu = use_gpu and torch.cuda.is_available()
-    device = torch.device("cuda" if use_gpu else "cpu")
-    print("Using device:", device)
-
-    model = Net(input_size, hidden_size, output_size, layer_num).to(device)
-    if optimizer_type == "sgd":
-        optimizer = apex.optimizers.FusedSGD(
-            model.parameters(),
-            lr=0.01,
-            set_grad_none=True,
+    for i in range(len(dataset)):
+        layer_num, input_size, hidden_size, output_size, batch_size = dataset[i]
+        elap, loss = run_single(
+            args, layer_num, input_size, hidden_size, output_size, batch_size
         )
-    else:
-        assert 0, "Unsupported optimizer type"
+        elap /= args.steps
 
-    if data_type == "float16":
-        apex.amp.initialize(
-            model, optimizer, opt_level="O2", verbosity=1
+        flops = batch_size * (
+            hidden_size * hidden_size * layer_num
+            + hidden_size * input_size
+            + hidden_size * output_size
         )
+        # Forward 2x and Backward 4x
+        flops *= 6
 
-    train(
-        model,
-        device,
-        train_loader,
-        optimizer,
-        data_type,
-        batch_size,
-        num_batches,
-        input_size,
-        hidden_size,
-        output_size,
-        layer_num,
-    )
+        QPS = batch_size / elap
+
+        print(
+            "{0:6},  {1:6},  {2:6},  {3:6},  {4:6},  {5:10.6f},  {6:8.1f}, {7:10.1f}".format(
+                layer_num,
+                input_size,
+                hidden_size,
+                output_size,
+                batch_size,
+                elap,
+                QPS,
+                flops / elap / 1.0e12,
+            )
+        )
 
 
 if __name__ == "__main__":
-    main()
+
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Measure the performance of MLP")
+    parser.add_argument("--device", required=True, choices=["cpu", "gpu", "tpu"])
+    parser.add_argument(
+        "--optimizer-type", default="sgd", help="Optimizer: SGD", choices=["sgd"]
+    )
+    parser.add_argument(
+        "--dtype",
+        default="float",
+        help="data type",
+        choices=["float", "float16", "bfloat16"],
+    )
+    parser.add_argument(
+        "--layer-num", type=int, default=20, help="Number of Linear layers"
+    )
+    parser.add_argument("--batch-size", type=int, default=512, help="Batch size")
+    parser.add_argument("--input-size", type=int, default=1024, help="Input layer size")
+    parser.add_argument(
+        "--hidden-size", type=int, default=128, help="Number of hidden_sizes per layer"
+    )
+    parser.add_argument(
+        "--output-size", type=int, default=1024, help="Output layer size"
+    )
+    # parser.add_argument("--num-batches", type=int, default=100, help="Number of batches to train")
+    parser.add_argument("--steps", type=int, default=100)
+    parser.add_argument("--warmups", type=int, default=10)
+
+    args = parser.parse_args()
+    layer_num = args.layer_num
+    batch_size = args.batch_size
+    input_size = args.input_size
+    hidden_size = args.hidden_size
+    output_size = args.output_size
+    # num_batches = args.num_batches
+
+    d = [(layer_num, input_size, hidden_size, output_size, batch_size)]
+    run(args, d)
