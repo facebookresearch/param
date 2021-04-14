fix Allgather output and BW calculation
Summary:
- report 'aggregated' sizes for Allgather instead of sizes per rank
- get correct tensor size for BW calculation and report

Differential Revision: D27756926

fbshipit-source-id: 2b0498f3aacdfe91b954c0cf43e3bd22be6cf43e

train/comms/pt/comms.py
@@ -202,13 +202,15 @@ class commsCollBench(paramCommsBench):
         elapsedTimeNS += (
             end - start
         ) * 1e9  # keeping time in NS, helps in divising data by nanoseconds
+
+        memSize = self.backendFuncs.get_mem_size(self.collectiveArgs)
+
         avgIterNS, algBW = comms_utils.getAlgBW(
-            elapsedTimeNS, self.collectiveArgs.dataSize, self.collectiveArgs.numIters
+            elapsedTimeNS, memSize, self.collectiveArgs.numIters
         )
         busBW = self.backendFuncs.getBusBW(
             self.collectiveArgs.collective, algBW, self.collectiveArgs.world_size
         )
-        memSize = self.backendFuncs.get_mem_size(self.collectiveArgs)
 
         self.backendFuncs.sync_barrier(self.collectiveArgs, desc="runColl_end")
         return (avgIterNS, algBW, busBW, memSize, x)
@@ -362,16 +364,8 @@ class commsCollBench(paramCommsBench):
             p75 = np.percentile(latencyAcrossRanks, 75)
             p95 = np.percentile(latencyAcrossRanks, 95)
 
-            self.collectiveArgs.dataSize = curSize
-            avgIterNS, algBW = comms_utils.getAlgBW(
-                p50 * 1e3, self.collectiveArgs.dataSize, self.collectiveArgs.numIters
-            )
-            busBW = self.backendFuncs.getBusBW(
-                self.collectiveArgs.collective, algBW, self.collectiveArgs.world_size
-            )
-
-            # refine busBW and algBW
-            busBW *= commsParams.bitwidth / 32.0
+            # adjust busBW
+            busBW = results[curSize]["busBW"] * (commsParams.bitwidth / 32.0)
 
             print(
                 "\tCOMMS-RES\t%12s\t%12s\t%12s\t%12s\t%12s\t%12s\t%12s"
@@ -381,7 +375,7 @@ class commsCollBench(paramCommsBench):
                     str("%.1f" % (p50)),
                     str("%.1f" % (p75)),
                     str("%.1f" % (p95)),
-                    str("%.3f" % (algBW)),
+                    str("%.3f" % (results[curSize]["algBW"])),
                     str("%.3f" % (busBW)),
                 )
             )

train/comms/pt/pytorch_dist_backend.py
@@ -276,9 +276,14 @@ class PyTorchDistBackend(backendFunctions):
 
     # Memory related
     def get_mem_size(self, collectiveArgs):
-        return (
-            collectiveArgs.ipTensor.nelement() * collectiveArgs.ipTensor.element_size()
-        )
+        _sizeBytes = 0
+        # opTensor could be a list of tensor for all_gather/gather, get the aggregated size
+        if isinstance(collectiveArgs.opTensor, list):
+            _sizeBytes = sum([t.nelement() * t.element_size() for t in collectiveArgs.opTensor])
+        else:
+            _sizeBytes = collectiveArgs.opTensor.nelement() * collectiveArgs.opTensor.element_size()
+
+        return _sizeBytes
 
     def alloc_random(self, sizeArr, curRankDevice="cuda", dtype=torch.float32, scaleFactor=1.0):
         if dtype in (torch.uint8, torch.int16, torch.int32, torch.long):