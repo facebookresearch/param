ff1082e4927a96b8948baa7d9428e8b107cc9d4a
Louis Feng <lofe@fb.com>
2021-10-23 13:53:11 -0700

    updated setup.py to match package install path.

--- train/compute/python/setup.py
+++ train/compute/python/setup.py
@@ -1,11 +1,22 @@
 from setuptools import setup
 
+package_base = "param_bench.train.compute.python"
+
+# Only list the top level packages and their dir mapping:
+# "install_destination_package_path": "source_dir_path"
+package_dir_map = {
+  f"{package_base}": ".",
+  f"{package_base}.lib": "lib",
+  f"{package_base}.workloads": "workloads",
+}
+
+packages = list(package_dir_map)
+
 setup(
     name='param_compute_lib',
     version='0.0.1',
     author='Louis Feng',
     author_email='lofe@fb.com',
-    packages=['param_bench.train.compute.lib', "param_bench.train.compute.lib.pytorch",
-              "param_bench.train.compute.workloads", "param_bench.train.compute.workloads.pytorch"],
-    package_dir={"param_bench.train.compute.lib": "lib", "param_bench.train.compute.workloads": "workloads"}
+    packages=packages,
+    package_dir=package_dir_map
 )
