f05fa45fb3377a5c94ba246a72dc75034f0996fc
Louis Feng <lofe@fb.com>
2022-06-02 22:13:05 -0700

    Add option to collect execution graph
    Summary: Collect execution graph for benchmarks.
    
    Differential Revision: D36404127
    
    fbshipit-source-id: f2e8bbd5bf5960c553e53f1ff70e7c74f250aec4

--- train/compute/python/lib/pytorch/cuda_util.py
+++ train/compute/python/lib/pytorch/cuda_util.py
@@ -8,7 +8,7 @@ logger = get_logger()
 def log_cuda_memory_usage():
     cuda_allocated = torch.cuda.memory_allocated() / 1048576
     cuda_reserved = torch.cuda.memory_reserved() / 1048576
-    logger.info(f"CUDA memory allocated = {cuda_allocated} MB, reserved = {cuda_reserved} MB")
+    logger.info(f"CUDA memory allocated = {cuda_allocated:.3f} MB, reserved = {cuda_reserved:.3f} MB")
 
 def free_torch_cuda_memory():
     gc.collect()
