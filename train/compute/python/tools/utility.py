0f1cc328ca73934a74958ab5a1cd962afec4a33a
Tae Kyung Heo <taekyungheo@meta.com>
2023-07-27 13:23:32 -0700

    Clean up utility.py
    Summary:
    * Reorganized package imports
    * Added type annotations
    * Added comments
    * Removed commented-out codes
    
    Reviewed By: briancoutinho
    
    Differential Revision: D47675917
    
    fbshipit-source-id: 617a82218d73695c363903fcc25cb93bd3035333

--- train/compute/python/tools/utility.py
+++ train/compute/python/tools/utility.py
@@ -1,16 +1,16 @@
+import datetime
 import gzip
 import json
 import logging
+import os
+import uuid
 from typing import Any, Dict
 
 from param_bench.train.compute.python.tools.execution_graph import ExecutionGraph
 
 
-def get_tmp_trace_filename():
-    import datetime
-    import os
-    import uuid
-
+def get_tmp_trace_filename() -> str:
+    """Generate a temporary filename using the current date, a UUID, and the process ID."""
     trace_fn = (
         "tmp_"
         + datetime.datetime.today().strftime("%Y%m%d")
@@ -23,19 +23,11 @@ def get_tmp_trace_filename():
     return trace_fn
 
 
-def trace_handler(prof):
+def trace_handler(prof: Any) -> None:
+    """Export a chrome trace"""
     fn = get_tmp_trace_filename()
     prof.export_chrome_trace("/tmp/" + fn)
     logging.warning(f"Chrome profile trace written to /tmp/{fn}")
-    # try:
-    #     from param_bench.train.compute.python.tools.internals import upload_trace
-
-    #     upload_trace(fn)
-    # except ImportError:
-    #     logging.info("FB internals not present")
-    # except Exception as e:
-    #     logging.info(f"Upload trace error: {e}")
-    #     pass
 
 
 def load_execution_trace_file(et_file_path: str) -> ExecutionGraph:
@@ -45,6 +37,7 @@ def load_execution_trace_file(et_file_path: str) -> ExecutionGraph:
 
 
 def read_dictionary_from_json_file(file_path: str) -> Dict[Any, Any]:
+    """Read a json file and return it as a dictionary."""
     with gzip.open(file_path, "rb") if file_path.endswith("gz") else open(
         file_path, "r"
     ) as f:
