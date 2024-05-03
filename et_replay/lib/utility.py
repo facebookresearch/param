88fcb586d121c7436bf8f5a95ffa8d482d307e7a
Taekyung Heo <7621438+TaekyungHeo@users.noreply.github.com>
2024-05-03 14:49:40 -0400

    Refactor param/utility.py

--- et_replay/lib/utility.py
+++ et_replay/lib/utility.py
@@ -10,7 +10,12 @@ from param.execution_trace import ExecutionTrace
 
 
 def get_tmp_trace_filename() -> str:
-    """Generate a temporary filename using the current date, a UUID, and the process ID."""
+    """
+    Generate a temporary filename using the current date, a UUID, and the process ID.
+
+    Returns:
+        str: A string representing the temporary trace filename.
+    """
     trace_fn = (
         "tmp_"
         + datetime.datetime.today().strftime("%Y%m%d")
@@ -24,31 +29,56 @@ def get_tmp_trace_filename() -> str:
 
 
 def trace_handler(prof: Any) -> None:
-    """Export a chrome trace"""
+    """
+    Handle the profiling data and export it as a Chrome trace file.
+
+    Args:
+        prof (Any): The profiler object with export capabilities.
+    """
     fn = get_tmp_trace_filename()
     prof.export_chrome_trace("/tmp/" + fn)
     logging.warning(f"Chrome profile trace written to /tmp/{fn}")
 
 
 def load_execution_trace_file(et_file_path: str) -> ExecutionTrace:
-    """Loads Execution Trace from json file and parses it."""
+    """
+    Load and parse an Execution Trace from a JSON file.
+
+    Args:
+        et_file_path (str): Path to the execution trace JSON file.
+
+    Returns:
+        ExecutionTrace: An instance of ExecutionTrace parsed from the file.
+    """
     data = read_dictionary_from_json_file(et_file_path)
     return ExecutionTrace(data)
 
 
 def read_dictionary_from_json_file(file_path: str) -> Dict[Any, Any]:
-    """Read a json file and return it as a dictionary."""
-    with (
-        gzip.open(file_path, "rb") if file_path.endswith("gz") else open(file_path, "r")
-    ) as f:
+    """
+    Read a JSON file and return its contents as a dictionary.
+
+    Args:
+        file_path (str): Path to the JSON file.
+
+    Returns:
+        Dict[Any, Any]: A dictionary representation of the JSON file.
+    """
+    with gzip.open(file_path, "rb") if file_path.endswith("gz") else open(file_path, "r") as f:
         return json.load(f)
 
 
 def write_dictionary_to_json_file(file_path: str, data: Dict[Any, Any]) -> None:
-    """Write input dictionary to a json file."""
+    """
+    Write a dictionary to a JSON file.
+
+    Args:
+        file_path (str): Path to the JSON file.
+        data (Dict[Any, Any]): The data to write to the file.
+    """
     if file_path.endswith("gz"):
-        with gzip.open(file_path, "w") as f:
-            f.write(json.dumps(data, indent=4).encode("utf-8"))
+        with gzip.open(file_path, "wt") as f:
+            f.write(json.dumps(data, indent=4))
     else:
         with open(file_path, "w") as f:
             json.dump(data, f, indent=4)
